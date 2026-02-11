e5d9c4724c76f22a83efcf21369172c054320dc9
Ajaraba5 <alejofreelancer@gmail.com>
2026-02-11 16:41:38 -0500

    Create authMiddleware.js for verifying JWT tokens

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Failed to authenticate token' });
+        }
+        // Save user information for use in other routes
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
